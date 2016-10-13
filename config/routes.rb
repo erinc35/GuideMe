7562f6075389dbc71bc71b0208d64c2c22a603ab
Johnny Choo <jchoo156@gmail.com>
2016-10-13 16:59:09 -0700

    creates Travelers controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/show'
-
-  get 'users/edit'
-
-  get 'users/update'
-
-  get 'users/destroy'
+  resources :travelers
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
