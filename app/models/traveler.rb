fa09a1d7a30debbe05a003c55f484dc3473ad8a4
Johnny Choo <jchoo156@gmail.com>
2016-10-14 13:15:11 -0700

    add code in config/environment.rb to stop error messages from messing up sign up/login forms

--- app/models/traveler.rb
+++ app/models/traveler.rb
@@ -3,5 +3,13 @@ class Traveler < ApplicationRecord
   has_many :reviews
   has_one :user, through: :trip
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :language, presence: true
+  validates :phone, presence: true
+
   has_secure_password
 end
