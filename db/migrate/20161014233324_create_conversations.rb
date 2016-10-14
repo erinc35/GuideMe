aa540882b5efa2a6f7ff48789474f37f88dca5b5
Asadullah Abdallah <asadullah@Asadullahs-MacBook-Air.local>
2016-10-14 16:45:13 -0700

    Create references to guides and travelers for conversations and messages migrations.

--- db/migrate/20161014233324_create_conversations.rb
+++ db/migrate/20161014233324_create_conversations.rb
@@ -1,6 +1,8 @@
 class CreateConversations < ActiveRecord::Migration[5.0]
   def change
     create_table :conversations do |t|
+      t.references :guide, index: true, foreign_key: true
+      t.references :traveler, index: true, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20161014233331_create_messages.rb
+++ db/migrate/20161014233331_create_messages.rb
@@ -1,7 +1,10 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-      t.string :body 
+      t.string :body
+      t.references :guide, index: true, foreign_key: true
+      t.references :traveler, index: true, foreign_key: true
+      t.references :conversation, index: true, foreign_key: true
 
       t.timestamps
     end
