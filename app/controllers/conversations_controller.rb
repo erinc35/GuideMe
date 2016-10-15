5c6ea9dd077846eecde33bd3d7ac1165318f0f91
Asadullah Abdallah <asadullah@Asadullahs-MacBook-Air.local>
2016-10-14 17:10:24 -0700

    Finish methods for messages controller.

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,2 +1,28 @@
 class ConversationsController < ApplicationController
+
+def create
+  @conversation = Chatroom.new(chatroom_params)
+  if @conversation.save
+    respond_to do |format|
+      format.html { redirect_to @conversation }
+      format.js
+    end
+  else
+    respond_to do |format|
+      flash[:notice] = {error: ["Guide is not online."]}
+      format.html { redirect_to :back }
+    end
+  end
+end
+
+def show
+  @chatroom = Chatroom.find_by(slug: params[:slug])
+  @message = Message.new
+end
+
+private
+
+  def chatroom_params
+    params.require(:chatroom).permit(:topic)
+  end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,20 @@
 class MessagesController < ApplicationController
-end
+
+    def create
+      @conversation = Conversation.find(params[:conversation_id])
+      @message = @conversation.messages.build(message_params)
+      if @message.save
+        ActionCable.server.broadcast 'messages',
+          message: @message.body,
+          traveler: @message.traveler.first_name
+          guide: @message.guide.first_name
+        head :ok
+      end
+    end
+
+    private
+
+      def message_params
+        params.require(:message).permit(:body)
+      end
+  end
