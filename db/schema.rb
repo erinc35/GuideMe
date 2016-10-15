d7568006a55d24d82c97e4c30e63a19d61098b51
APilarca <angelpill15@gmail.com>
2016-10-15 14:41:39 -0700

    add columns to trip table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161015202904) do
-# ActiveRecord::Schema.define(version: 20161014233331) do
+ActiveRecord::Schema.define(version: 20161015214010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "add_start_date_and_end_date_to_trips", force: :cascade do |t|
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "available_dates", force: :cascade do |t|
     t.datetime "start_date"
@@ -91,6 +96,8 @@ ActiveRecord::Schema.define(version: 20161015202904) do
     t.text     "requests"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.datetime "start_date"
+    t.datetime "end_date"
     t.index ["guide_id"], name: "index_trips_on_guide_id", using: :btree
     t.index ["traveler_id"], name: "index_trips_on_traveler_id", using: :btree
   end
