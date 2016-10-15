36e11cb44a3ced72535242da0062fa2a3033d8f0
Johnny Choo <jchoo156@gmail.com>
2016-10-15 15:24:36 -0700

    add signup, login

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161014233331) do
+ActiveRecord::Schema.define(version: 20161015003435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "conversations", force: :cascade do |t|
     t.string   "topic"
-
     t.string   "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -47,6 +46,15 @@ ActiveRecord::Schema.define(version: 20161014233331) do
     t.index ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
     t.index ["guide_id"], name: "index_messages_on_guide_id", using: :btree
     t.index ["traveler_id"], name: "index_messages_on_traveler_id", using: :btree
+  end
+
+  create_table "registrations", force: :cascade do |t|
+    t.text     "notification_params"
+    t.string   "status"
+    t.string   "transaction_id"
+    t.datetime "purchased_at"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
   end
 
   create_table "reviews", force: :cascade do |t|
