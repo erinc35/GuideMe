a724029915da5c29e4a375c7b80a8069a8bc79e6
Asadullah Abdallah <asadullah@Asadullahs-MacBook-Air.local>
2016-10-14 21:33:41 -0700

    Comment out controller for message.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,32 +1,32 @@
-class MessagesController < ApplicationController
-
-  def create
-    message = Message.new(message_params)
-    if current_guide
-      message.guide = current_guide
-      message.traveler =
-    elsif current_traveler
-      message.guide =
-      message.traveler = current_traveler
-    else
-      redirect_to conversations_path
-      alert("Your not signed in to chat with a traveler or guide.")
-    end
-
-    if message.save
-      ActionCable.server.broadcast 'messages',
-      message: message.body,
-      guide: message.guide.first_name
-      traveler: message.traveler.first_name
-      head :ok
-    else
-      redirect_to conversations_path
-    end
-  end
-
-  private
-
-    def message_params
-      params.require(:message).permit(:content, :conversation_id)
-    end
-end
+# class MessagesController < ApplicationController
+#
+#   def create
+#     message = Message.new(message_params)
+#     if current_guide
+#       message.guide = current_guide
+#       message.traveler =
+#     elsif current_traveler
+#       message.guide =
+#       message.traveler = current_traveler
+#     else
+#       redirect_to conversations_path
+#       alert("Your not signed in to chat with a traveler or guide.")
+#     end
+#
+#     if message.save
+#       ActionCable.server.broadcast 'messages',
+#       message: message.body,
+#       guide: message.guide.first_name
+#       traveler: message.traveler.first_name
+#       head :ok
+#     else
+#       redirect_to conversations_path
+#     end
+#   end
+#
+#   private
+#
+#     def message_params
+#       params.require(:message).permit(:content, :conversation_id)
+#     end
+# end
