5500baa3b48f88b3aa78351d158e3b8935653adb
Asadullah Abdallah <asadullah@Asadullahs-MacBook-Air.local>
2016-10-15 15:03:58 -0700

    Create messsages partial, create messenger action cable call with polymorphic associations.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,22 +3,23 @@ class MessagesController < ApplicationController
   def create
     message = Message.new(message_params)
     if current_guide
-      message.guide = current_guide
-      p message.valid?
+      message.messenger_id = current_guide.id
+      message.messenger_type = current_guide.class
       if message.save
         ActionCable.server.broadcast 'messages',
         message: message.body,
-        guide: message.guide.first_name
+        guide: message.messenger.first_name
         head :ok
       else
         redirect_to conversations_path
       end
     elsif current_traveler
-      message.traveler = current_traveler
+      message.messenger_id = current_traveler.id
+      message.messenger_type = current_traveler.class
       if message.save
         ActionCable.server.broadcast 'messages',
         message: message.body,
-        traveler: message.traveler.first_name
+        traveler: message.messenger.first_name
         head :ok
       else
         redirect_to conversations_path
