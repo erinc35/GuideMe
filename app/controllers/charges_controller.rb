fab8dab4eb9abcc92c9ba765ca28864e176d11f5
Johnny Choo <jchoo156@gmail.com>
2016-10-15 20:49:38 -0700

    can access stripes credit card info page and enter in test credit card number to submit a payment

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,2 +1,5 @@
 class ChargesController < ApplicationController
+  def new
+    @guide = Guide.find(params[:guide_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'unavailable_dates/show'
-
-  get 'unavailable_dates/new'
-
-  get 'unavailable_dates/create'
-
-  get 'unavailable_dates/update'
-
-  get 'unavailable_dates/destroy'
-
-  get 'available_dates/show'
-
-  get 'available_dates/new'
-
-  get 'available_dates/create'
-
-  get 'available_dates/update'
-
-  get 'available_dates/destroy'
-
   resource :sessions
 
-  resources :guides
+  resources :guides do
+    resources :charges
+  end
+
 
   resources :travelers
 
