6849c6f1f2ecc0bbb9fb38b085e6221ae93fc38d
APilarca <angelpill15@gmail.com>
2016-10-17 14:19:39 -0700

    create seed data for guides and travelers

--- app/models/traveler.rb
+++ app/models/traveler.rb
@@ -5,13 +5,13 @@ class Traveler < ApplicationRecord
   has_many :conversations, through: :messages
   has_one :guide, through: :trip
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
-  validates :language, presence: true
-  validates :phone, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # validates :email, presence: true
+  # validates :password, presence: true
+  # validates :password_confirmation, presence: true
+  # validates :language, presence: true
+  # validates :phone, presence: true
 
   has_secure_password
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,19 @@
 require 'faker'
 
 Traveler.destroy_all
+# Guide.destroy_all
 
-5.times do
-	Traveler.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "password", password_confirmation: "password", phone: Faker::PhoneNumber.cell_phone, photo: Faker::Avatar.image)
+languages = ["English", "Spanish", "German", "French", "Italian", "Portuguese", "Japanese", "Korean", "Turkish", "Mandarin", "Cantonese"]
+locations = ["San Francisco"]
+
+25.times do
+	travelers = Traveler.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "password", password_confirmation: "password", phone: Faker::PhoneNumber.cell_phone, photo: Faker::Avatar.image)
 end
 
-# first_name: Bob, last_name: Bob, email: Bob@bob.com, password: "password", password_confirmation: "password", phone: Faker::PhoneNumber.cell_phone
+50.times do
+	guides = Guide.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "password", password_confirmation: "password", phone: Faker::PhoneNumber.cell_phone, language: languages.sample, location: locations.sample, has_car: "yes", photo: Faker::Avatar.image)
+end
+
+
+
+# Traveler.create(first_name: "Bob", last_name: "Bob", email: "Bob@bob.com", password: "password", phone: "1234567")
