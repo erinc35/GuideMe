a2e13efe7f3a0bbc6cf56d2c886e4a71effe959a
Johnny Choo <jchoo156@gmail.com>
2016-10-17 16:11:30 -0700

    fix ajax issue on guides index page

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -20,7 +20,16 @@ require 'yelp'
      @language = params[:language]
 
     ##########---------YELP---------##########
-    @api_call = Yelp.client.search(@location, { term: 'events', limit: 16 }).businesses
+    p "-" * 200
+    p @events_call = Yelp.client.search(@location, { term: 'events', limit: 16 }).businesses
+    p "*" * 200
+    p @events_call[0].image_url
+    p "*" * 200
+    p "-" * 200
+    p @restaurants_call = Yelp.client.search(@location, { term: 'restaurants', limit: 16 }).businesses
+    p "-" * 200
+    p @monuments_call = Yelp.client.search(@location, { term: 'monuments', limit: 16 }).businesses
+    p "-" * 200
 
   end
 
