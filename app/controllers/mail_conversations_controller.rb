c9193e450737458b4135a9b8ef85c5a40960cbcb
Asadullah Abdallah <asadullah@Asadullahs-MacBook-Air.local>
2016-10-17 23:59:17 -0700

    Create ability for user to send a new message to another user.

--- app/controllers/mail_conversations_controller.rb
+++ app/controllers/mail_conversations_controller.rb
@@ -4,6 +4,16 @@ class MailConversationsController < ApplicationController
     @mailbox_conversations = current_user.mailbox.conversations
   end
 
+  def new
+    @recipients = Guide.all - [current_user]
+  end
+
+  def create
+    recipient = Guide.find(params[:guide_id])
+    receipt = current_user.send_message(recipient, params[:body], params[:subject])
+    redirect_to mail_conversation_path(receipt.conversation)
+  end
+
   def show
     @mailbox_conversation = current_user.mailbox.conversations.find(params[:id])
   end
--- app/controllers/mail_messages_controller.rb
+++ app/controllers/mail_messages_controller.rb
@@ -1,2 +1,14 @@
 class MailMessagesController < ApplicationController
+  before_action :set_mail_conversation
+
+  def create
+    receipt = current_user.reply_to_conversation(@mail_conversation, params[:body])
+    redirect_to mail_conversation_path(receipt.conversation)
+  end
+
+  private
+
+  def set_mail_conversation
+    @mail_conversation = current_user.mailbox.conversations.find(params[:mail_conversation_id])
+  end
 end
