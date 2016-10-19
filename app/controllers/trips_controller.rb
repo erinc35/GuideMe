48e298deb56da386bc47d177976f5162925cc738
Johnny Choo <jchoo156@gmail.com>
2016-10-18 17:44:08 -0700

    checkout button on guides index page and that links to trips show page

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -14,6 +14,11 @@ class TripsController < ApplicationController
     @trip = Trip.new(location: params[:location], start_date: params[:start_date], end_date: params[:end_date])
     @trip.guide = Guide.find_by(email: session["guide"])
 
+    if session[:traveler_id]
+      @traveler = Traveler.find(session[:traveler_id])
+    else
+      redirect_to new_traveler_path
+    end
   end
 
   def edit
