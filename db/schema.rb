d0da0b601a3e783736085841e733ab171fc49b47
APilarca <angelpill15@gmail.com>
2016-10-19 22:59:34 -0700

    make paperclip file upload possible

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161020051806) do
+ActiveRecord::Schema.define(version: 20161020053153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
