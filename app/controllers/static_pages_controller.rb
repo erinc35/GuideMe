0ae4cdedfd108904a8a1b69e00943c5fe9e0d732
Johnny Choo <jchoo156@gmail.com>
2016-10-25 19:54:17 -0700

    get traveler sign up modal to work on every page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,5 @@
 class StaticPagesController < ApplicationController
   def index
-    p "838383___" * 200
-    p session[:traveler_id]
     @languages = %w(English Spanish German French Italian Portuguese Japanese Korean Turkish Mandarin Cantonese)
   end
 end
