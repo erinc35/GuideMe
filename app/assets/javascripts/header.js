40d4f866b723a689785dc85031fbecb176653ce5
Johnny Choo <jchoo156@gmail.com>
2016-12-19 21:57:59 -0800

    fix hover on navbar

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,15 +1,40 @@
 $(document).ready(function(){
- loginFormPress();
+  loginFormPress();
+   // mouseLeaveBlack();
 
 });
 
 function loginFormPress()  {
-   $(".login-link").on("click", function(event){
-    event.preventDefault();
-    $(".login-form").toggle();
+  $(".login-link").on("click", function(event){
+   event.preventDefault();
+   $(".login-form").toggle();
+   mouseLeaveBlack();
+   mouseEnterWhite();
+ })
 
-  })
+}
 
+
+function mouseLeaveBlack(){
+ $(".login-link").on("mouseleave", function(){
+
+
+   $(".login-link").css("background-color", "#333333 ")
+   // alert("fffff")
+   $(".login-link").css("color", "beige");
+
+ })
+}
+
+function mouseEnterWhite(){
+ $(".login-link").on("mouseenter", function(){
+
+
+   $(".login-link").css("background-color", "white")
+   // alert("fffff")
+   $(".login-link").css("color", "#333333 ")
+
+ })
 }
 
 
--- app/assets/javascripts/signup_drop.js
+++ app/assets/javascripts/signup_drop.js
@@ -1,12 +1,30 @@
 $(document).ready(function(){
- signUpHover();
+signUpHover();
 
 });
 
 function signUpHover()  {
-   $(".signup-link").on("click", function () {
-     $('.signup-drop').toggle()
-   }
-  )
+  $(".signup-link").on("click", function () {
+    $('.signup-drop').toggle()
+  })
+  mouseLeaveBlack2();
+  mouseEnterWhite2();
+}
 
+function mouseLeaveBlack2(){
+ $(".signup-link").on("mouseleave", function(){
+   $(".signup-link").css("background-color", "#333333 ")
+   $(".signup-link").css("color", "beige");
+
+ })
+}
+
+
+
+function mouseEnterWhite2(){
+ $(".signup-link").on("mouseenter", function(){
+   $(".signup-link").css("background-color", "white")
+   $(".signup-link").css("color", "#333333 ")
+
+ })
 }
