a35e1ce8eb7acc4b43888e1b3bb373d53716bbd7
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2016-12-24 11:27:22 -0800

    checkout login button linked to sign up form

--- app/assets/javascripts/checkout.js
+++ app/assets/javascripts/checkout.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
-
+  checkoutLogin();
 })
 
-
+function checkoutLogin(){
+  $("#checkout-login-but").on("click", function(e){
+    event.preventDefault();
+    $(".checkout-signup-atts").css("display", "none");
+     $(".checkout-login-form").css("display", "block");
+  })
+}
