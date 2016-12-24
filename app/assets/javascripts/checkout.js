98dd637c60bd1df6cb9f3189c9f7a36f83aacd9b
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2016-12-24 11:29:58 -0800

    checkout login and signup buts linked eachother

--- app/assets/javascripts/checkout.js
+++ app/assets/javascripts/checkout.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   checkoutLogin();
+  checkoutSignup();
 })
 
 function checkoutLogin(){
@@ -7,5 +8,14 @@ function checkoutLogin(){
     event.preventDefault();
     $(".checkout-signup-atts").css("display", "none");
      $(".checkout-login-form").css("display", "block");
+  })
+}
+
+
+function checkoutSignup(){
+  $("#checkout-signup-but").on("click", function(e){
+    event.preventDefault();
+     $(".checkout-login-form").css("display", "none");
+    $(".checkout-signup-atts").css("display", "block");
   })
 }
